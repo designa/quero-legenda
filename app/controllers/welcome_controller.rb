ece64ddd3a74950b23d33cf4e3a647f80777153a
oieduardorabelo <oieduardorabelo@gmail.com>
2013-09-28 15:23:03 -0300

    top nine bilbor

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @most_viewed = CLIENT.videos_by(:most_viewed, per_page: 5)
+    @most_viewed = CLIENT.videos_by(:most_viewed, per_page: 9)
   end
 end

e75b904251f73077bfbea43d3d213165814396b9
oieduardorabelo <oieduardorabelo@gmail.com>
2013-09-28 16:08:00 -0300

    Charmander, burn all this shit

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -5,6 +5,7 @@ class SearchsController < ApplicationController
 
   def result
     @title = params[:search][:title]
+    @subtitle = Subtitle.new
     get_results(@title)
   end
 
@@ -12,7 +13,7 @@ class SearchsController < ApplicationController
   protected
 
   def get_results(title)
-    @videos = CLIENT.videos_by(query: title)
+    @videos = CLIENT.videos_by(query: title, per_page: 9)
   end
 
 end
